ab5a5d0577542d4fab1a3ce9cf0b19c5a37bd55b
Ex99 <mregm99909@gmail.com>
2021-01-26 22:13:23 -0600

    Se corrigieron cambios en el proyecto

--- Program.cs
+++ Program.cs
@@ -6,25 +6,53 @@ using BenchmarkDotNet.Running;
 namespace proyecto_lyaii
 {
     [HtmlExporter]
+    [MemoryDiagnoser]
     [DisassemblyDiagnoser(printSource: true)]
     [RyuJitX64Job]
     public class OperacionCodigo
     {
         // Es opcional el uso de campos globales
-        int[] field = Enumerable.Range(0, 100).ToArray();
+        int[] campo = Enumerable.Range(0, 100).ToArray();
+
+        // Escribir código ...
 
         [Benchmark]
-        public int main()
+        public int SumaLocal()
         {
-            // Escribe código aqui...
-            var local = field;
+            var local = campo;
 
             int sum = 0;
             for (int i = 0; i < local.Length; i++)
                 sum += local[i];
 
-            // No te olvides del return
             return sum;
+        }
+
+        [Benchmark]
+        public int SumaCampo()
+        {
+            int sum = 0;
+            for (int i = 0; i < campo.Length; i++)
+                sum += campo[i];
+
+            return sum;
+        }
+
+        // Metodos rápidos
+        [Benchmark]
+        public byte[] ArregloVacio() => Array.Empty<byte>();
+
+        [Benchmark]
+        public byte[] OchoBytes() => new byte[8];
+
+        [Benchmark]
+        public byte[] UnLinq()
+        {
+            return Enumerable
+                .Range(0, 100)
+                .Where(i => i % 2 == 0)
+                .Select(i => (byte)i)
+                .ToArray();
         }
     }
 
